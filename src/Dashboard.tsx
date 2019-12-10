308faa8315205e402fd37d17f04d20dacdfe8e72
Mark Ritchie <mwritchie2@gmail.com>
2019-12-09 19:47:52 -0600

    created dashboard component

---
+++ src/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Dashboard() {
+  return (
+    <Typography variant='h2' >Dngn Dash</Typography>
+  )
+}
+
+export default Dashboard;
