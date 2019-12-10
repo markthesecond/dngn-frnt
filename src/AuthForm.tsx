57cf61c8891bc6c1a55dfc7b761bf7a9d4310882
Mark Ritchie <mwritchie2@gmail.com>
2019-12-10 01:34:41 -0600

    added form input and state

--- src/AuthForm.tsx
+++ src/AuthForm.tsx
@@ -2,14 +2,52 @@ import React, { ReactElement, useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 function AuthForm(): ReactElement {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signedUp, setSignedUp] = useState(true);
+  const handleClick = () => {setSignedUp(!signedUp)};
+
+  const usernameField = signedUp
+    ? null
+    : <TextField
+        label='Username'
+        type='username'
+        name='username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)} />
+  const authSwitchText = signedUp
+    ? <>Need an account Click <span onClick={handleClick}>here</span>.</>
+    : <>Already signed up? Click <span onClick={handleClick}>here</span>.</>
+
   return (
     <Paper>
-      <Typography variant='h4'>Sign In</Typography>
-      <Box component='form'>
-
+      <Typography variant='h4'>Sign {signedUp ? 'In' : 'Up'}</Typography>
+      <Box component='form' >
+        {usernameField}<br/>
+        <TextField
+          label='Email'
+          type='email'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} /><br/>
+        <TextField
+          label='Password'
+          type='password'
+          name='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} /><br/>
+        <Button
+          color='primary'
+          variant='contained' >
+          {signedUp ? 'Log In' : 'Register'}
+        </Button>
       </Box>
+      <Typography variant='subtitle1'>{authSwitchText}</Typography>
     </Paper>
   )
 }
