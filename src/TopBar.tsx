b062f6b3221f9bcd2fc91654c343018a3bc24b65
Mark Ritchie <mwritchie2@gmail.com>
2020-02-20 08:11:36 -0600

    added topbar w/ logout button

---
+++ src/TopBar.tsx
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { USER_LOGOUT } from './graphql/authQueries';
+import { DngnCntxt } from './App';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  toolbar: {
+    alignItems: 'center',
+    paddingTop: theme.spacing(1),
+    // paddingBottom: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    alignSelf: 'flex-end',
+  },
+  authButton: {
+    marginLeft: 'auto',
+  },
+}))
+
+export default function TopBar(props: any): React.ReactElement {
+  const classes = useStyles();
+  const [ logout/*, { data } */ ] = useMutation(USER_LOGOUT);
+  const { loggedIn, setLoggedIn, setCurrentUser } = useContext(DngnCntxt);
+
+  const handleLogout = (): void => {
+    logout();
+    setLoggedIn(false);
+    setCurrentUser({ username: '', _id: '' });
+  }
+  const logoutButton = loggedIn
+    ? <Button
+        onClick={ handleLogout }
+        className={ classes.authButton } >
+        Logout
+      </Button>
+    : null // don't show a button if not logged in
+
+  return (
+    <div className={ classes.root } >
+      <AppBar position='static'>
+        <Toolbar className={ classes.toolbar } >
+          <Typography
+            variant='h4' >
+            DngnBddy
+          </Typography>
+          { logoutButton }
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
