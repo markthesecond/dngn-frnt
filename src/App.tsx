8455b818d101c230b05b748327957c6244c6dd2a
Mark Ritchie <mwritchie2@gmail.com>
2020-03-04 17:47:59 -0600

    modularized Apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,13 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from '@apollo/react-hooks';
 import Dashboard from './Dashboard';
 import AuthForm from './AuthForm';
 import CharacterContainer from './CharacterContainer';
 import TopBar from './TopBar';
 import BottomBar from './BottomBar';
+import client from './apolloClient';
 
 export interface CurrentUser {
   username?: string,
@@ -40,25 +36,6 @@ const dummyContext = {
 }
 
 export const DngnCntxt: React.Context<IDngnCntxt> = createContext<IDngnCntxt>({...dummyContext});
-
-const client: ApolloClient<any> = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message ${message}, Location: ${locations}, Path: ${path}`,
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-      credentials: 'include',
-    })
-  ]),  
-  cache: new InMemoryCache(),
-});
 
 const App: React.FC = () => {
   const [loggedIn, setLoggedIn] = useState(false);
---
+++ src/apolloClient.ts
@@ -0,0 +1,26 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message ${message}, Location: ${locations}, Path: ${path}`,
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+      credentials: 'include',
+    }),
+  ]),  
+  cache: new InMemoryCache(),
+});
+
+export default client
