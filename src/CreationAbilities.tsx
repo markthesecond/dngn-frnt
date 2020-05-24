2748b6e3eee042cd9a9f297edc8527745cbc8fa4
Mark Ritchie <mwritchie2@gmail.com>
2020-05-24 00:00:07 -0500

    cleaned up file
    Moved IAbilites (prev: interface Abilities) to util/character.ts.
    Added interface for panel props.
    Added pre-filled display of abilities w/ bonuses
    applied (AbilityList).
    Began support for assigning PC choice ability bonuses.
    Improved handleAbilityChange, abilties state handling.

--- src/CreationAbilities.tsx
+++ src/CreationAbilities.tsx
@@ -3,40 +3,52 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import AbilitySelect from './AbilitySelect';
-import { CharacterModel } from './util/character';
+import AbilityList from './AbilityList';
+import { createAbilities, IAbilities, CharacterModel, AbilityBonusModel } from './util/character';
 
-interface Abilities {
-  STR?: number | null,
-  DEX?: number,
-  CON?: number,
-  INT?: number,
-  WIS?: number,
-  CHA?: number
+const abilitiesDescription = `Your character's ability scores measure how readily
+  they're able to meet challenges of different types. Each class relies on
+  different abilities to perform the various actions identifying each, e.g.
+  fighters need high strength to hit and do extra damage, while wizards need
+  high intelligence to have more and more effective spells.`
+export const abilityNames = ['STR','DEX','CON','INT','WIS','CHA'];
+
+interface CreationAbilitiesProps {
+  choices: CharacterModel,
+  setChoices: React.Dispatch<React.SetStateAction<CharacterModel>>
 }
 
 /**
  * Panel for selecting abilities and bonuses
  */
-function CreationAbilities({choices, setChoices}: any) {
-  const abilitiesDescription = `Your character's ability scores measure how readily
-    they're able to meet challenges of different types. Each class relies on
-    different abilities to perform the various actions identifying each, e.g.
-    fighters need high strength to hit and do extra damage, while wizards need
-    high intelligence to have more and more effective spells.`
-  const [description] = useState<string>(abilitiesDescription);
-  const [ /* abilities */, setAbilities ] = useState<Abilities>({})
-  const [scores] = useState<Array<number>>([15,14,13,12,10,8])
-  const abilityNames = ['STR','DEX','CON','INT','WIS','CHA'];
+function CreationAbilities({choices, setChoices}: CreationAbilitiesProps) {
+  const [ description ] = useState<string>(abilitiesDescription);
+  const [ abilities, setAbilities ] = useState<IAbilities>(() => {
+    if (choices.abilities) {
+      return choices.abilities;
+    } else {
+      return createAbilities();
+    }
+  });
+  const [ scoreChoices ] = useState<Array<number>>([15,14,13,12,10,8]);
+  const [ bonuses, /* setBonuses */ ] = useState<Array<AbilityBonusModel>>(() => {
+    if (choices.race) {
+      return choices.race.abilityBonuses;
+    } else {
+      return [];
+    }
+  });
   const handleAbilityChange = (event: React.ChangeEvent<{name: string | undefined, value: any}>): void => {
     const abilName: string = event.target.name ? event.target.name : 'F';
-    setAbilities(abilities => {
-      const abils = {...abilities, [abilName]: event.target.value};
-      setChoices((choices: CharacterModel) => {return {...choices, abilities: abils}})
-      return abils
+    const abilVal = parseInt(event.target.value);
+    const abils: IAbilities = { ...abilities, [abilName]: abilVal ? abilVal : 8 };
+    setAbilities(abils);
+    setChoices(() => {
+      return { ...choices, abilities: abils }
     });
   }
 
-  const options = scores.map((s, i) => <option key={i} value={s} >{s}</option> );
+  const options = scoreChoices.map((s, i) => <option key={i} value={s} >{s}</option> );
   const abilitySelects = abilityNames.map(a => {
     return <AbilitySelect
       key={a}
@@ -49,6 +61,7 @@ function CreationAbilities({choices, setChoices}: any) {
     <Paper>
       <Grid container >
         {abilitySelects}
+        <AbilityList scores={ abilities } bonuses={ bonuses } />
         <Typography variant='body1' >
           {description}
         </Typography>
