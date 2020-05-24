047ab2562ebc719bb25832b79982a22c35eacd53
Mark Ritchie <mwritchie2@gmail.com>
2020-05-23 23:29:23 -0500

    created file for queries re friends + groups

---
+++ src/graphql/userQueries.ts
@@ -0,0 +1,23 @@
+import gql from 'graphql-tag';
+
+export const GET_FRIENDS = gql`
+  query FriendsAndCharacters($_id: MongoID!) {
+    userById(_id: $_id) {
+      friends {
+        _id
+        username
+        email
+      }
+      characters {
+        _id
+        name
+        class {
+          name
+        }
+        race {
+          name
+        }
+      }
+    }
+  }
+`
