94b7d069c4051ae2e47a9e1bdd21249ba9af20a7
Mark Ritchie <mwritchie2@gmail.com>
2020-05-24 00:41:34 -0500

    moved auth queries to dedicated file

--- src/graphql/authQueries.ts
+++ src/graphql/authQueries.ts
@@ -21,3 +21,36 @@ export const USER_LOGOUT = gql`
   }
   ${fragments.userInfo}
 `
+
+export const LOGIN = gql`
+  mutation UserLogin($email: String!, $password: String!) {
+    userLogin(email: $email, password: $password) {
+      user {
+        ...userInfo
+      }
+      token
+    }
+  }
+  ${fragments.userInfo}
+`
+
+export const REGISTER = gql`
+  mutation UserRegister($username: String, $email: String!, $password: String!) {
+    userRegister(username: $username, email: $email, password: $password) {
+      user {
+        ...userInfo
+      }
+      token
+    }
+  }
+  ${fragments.userInfo}
+`
+
+export const ME = gql`
+  query Me {
+    me {
+      ...userInfo
+    }
+  }
+  ${fragments.userInfo}
+`
