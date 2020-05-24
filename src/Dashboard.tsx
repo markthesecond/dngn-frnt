88960c61e6d2cd743cbc40bcbdd14eb6417fb684
Mark Ritchie <mwritchie2@gmail.com>
2020-05-23 23:31:01 -0500

    moved GET_FRIENDS and removed cruft

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,9 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { DngnCntxt, IDngnCntxt } from './App';
 import {
-  // BrowserRouter as Router,
-  // Switch,
-  // Route,
   Link
 } from 'react-router-dom';
 import DashList from './DashList';
@@ -12,39 +9,17 @@ import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
-import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import { IUser } from './util/user';
-
-const GET_FRIENDS = gql`
-  query FriendsAndCharacters($_id: MongoID!) {
-    userById(_id: $_id) {
-      friends {
-        _id
-        username
-        email
-      }
-      characters {
-        _id
-        name
-        class {
-          name
-        }
-        race {
-          name
-        }
-      }
-    }
-  }
-`
+import { GET_FRIENDS } from './graphql/userQueries';
 
 function Dashboard(): React.ReactElement {
-  // const friendAdd = 
-  // const [addingEntity, setAddingEntity] = useState('friend');
-  // const [adding, setAdding] = useState(false);
   const [friends, setFriends] = useState<Array<IUser>>([]);
   const dngnContext: IDngnCntxt = useContext(DngnCntxt);
-  const {loading, data} = useQuery(GET_FRIENDS, {variables: {_id: dngnContext.currentUser._id}})
+  const {loading, data} = useQuery(
+    GET_FRIENDS,
+    { variables: { _id: dngnContext.currentUser._id } }
+  );
   
   useEffect(() => {
     if (data) setFriends(data.userById.friends)
